3a51d4fb8347fac95fd19e90533f11595e42a423
Terry Gunderson <tgunderson25@gmail.com>
2023-08-03 15:17:46 -0500

    Creating base character class and setup

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright SericoGames
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright SericoGames
+
+#pragma once
+
+#include "CoreMinimal.h"
+
